ad566121124968b0b41dccb67e3edf6f5f3a2ddd
sergio110304 <105168888+sergio110304@users.noreply.github.com>
2022-10-06 09:28:04 -0500

    Add files via upload

---
+++ implementación.py
@@ -0,0 +1,19 @@
+class Tablero: 
+    def __init__(self, minas:str, numeros:str):
+        self.minas = minas
+        self.numeros = numeros
+
+class Numeros: 
+    def alrededor(self):
+        pass
+
+class Minas: 
+    def __init__(self, posicion:str):
+        self.posicion = posicion
+    def explotar(self):
+        pass
+
+class Jugador: 
+    def seleccionar(self):
+        pass
+        
